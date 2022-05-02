d90791c1e29747c9f1cf0b82274ff598ebbb5287
Matheus <matheusfsantos.152@gmail.com>
2022-05-02 11:08:49 -0300

    add

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
+    public bool pause;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +14,20 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && pause == false)
+        {
+            pause = true;
+            Time.timeScale = 0;
+            SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+        }
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Land");
+        // SceneManager.LoadScene("Land");
+        pause = false;
+        Time.timeScale = 1;
+        SceneManager.UnloadScene("Menu");
     }
 
     public void Options()
