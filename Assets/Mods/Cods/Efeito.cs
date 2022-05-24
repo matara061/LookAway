46ff4bcb0950a0a59e5a33f85aaab2f923dca71e
Joao0103 <88664663+Joao0103@users.noreply.github.com>
2022-05-24 11:18:41 -0300

    Update Shrines

---
+++ Assets/Mods/Cods/Efeito.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alavanca : MonoBehaviour
+{
+
+    public GameObject Effect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Instância o efeito
+        Instantiate(Effect, transform.position, transform.rotation);
+    }
+
+}
